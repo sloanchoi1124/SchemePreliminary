5a008bbf5d65f550c65d85c7814bead167184957
sloanchoi1124 <sloan.xuelai.cui@gmail.com>
2014-06-23 12:21:47 -0400

    usage for testing.java updated

--- Scheme_Preliminary/src/testing/testing.java
+++ Scheme_Preliminary/src/testing/testing.java
@@ -1,5 +1,14 @@
 package testing;
+//FOR TESTING CLASS
+//this is a class where you can testing.java(or other java files if needed) as java application
 
+//In order to run testing.java itself as an java application
+//1) right click -> properties ->run/debug settings-> 
+//2) if there's testing already, click edit
+     //if there's not, click add->choose java application
+//3) go to class path, remove the Android 4.4 library
+//4) click run
+//5) In order to run the android application after running the java application, run configuration may need to be edited
 public class testing {
 	public static void main(String args[])
 	{
