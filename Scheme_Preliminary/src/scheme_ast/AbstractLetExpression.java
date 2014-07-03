90071d5889e2bce4d00a80fc5900cb90daf27387
Chunyi Lyu <chunyineko@gmail.com>
2014-07-03 14:30:17 -0400

    updated letExpression, parser, evaluator, mapper

---
+++ Scheme_Preliminary/src/scheme_ast/AbstractLetExpression.java
@@ -0,0 +1,25 @@
+package scheme_ast;
+
+import java.util.List;
+
+import util.Pair;
+
+public abstract class AbstractLetExpression extends Expression{
+	
+	private List<Pair<String, Expression>> mBindings; 
+	private Expression mBody;
+	
+	public AbstractLetExpression(List<Pair<String, Expression>> bindings, Expression body) {
+		super();
+		mBindings = bindings;
+		mBody = body;
+	}
+	
+	public List<Pair<String, Expression>> getBindings() {
+		return mBindings;
+	}
+	
+	public Expression getBody() {
+		return mBody;
+	}
+}
