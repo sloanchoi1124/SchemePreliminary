b8885daa125ccd986cc9e93f10827f4cf06d5794
Chunyi Lyu <chunyineko@gmail.com>
2014-07-25 16:48:07 -0400

    ConsExpression with some list operation methods

--- Scheme_Preliminary/src/scheme_ast/ConsExpression.java
+++ Scheme_Preliminary/src/scheme_ast/ConsExpression.java
@@ -1,5 +1,14 @@
 package scheme_ast;
 
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+
+import parser.Lexer;
+import parser.Parser;
+import parser.token.Token;
+import evaluator.Evaluator;
+import unparser.Unparser;
 import util.Pair;
 
 public class ConsExpression extends Expression{
@@ -19,8 +28,51 @@ public class ConsExpression extends Expression{
 		return this.cell.second;
 	}
 	
+	public BigInteger length() {
+		BigInteger result = BigInteger.valueOf(length(this, 0));
+		return result;
+	}
+	
+	private static int length(ConsExpression c, int helper) {
+		if (c == null) {
+			return helper;
+		} else if (! (c.cdr() instanceof ConsExpression)){
+			return helper + 1;
+		} else {
+			return length( (ConsExpression)c.cdr(), helper + 1);
+		}
+	}
+	
+	public ConsExpression reverse() {
+		return reverse(this);
+	}
+	
+	private ConsExpression reverse(ConsExpression c) {
+		ConsExpression temp = c;
+		ConsExpression result = new ConsExpression(temp.car(), new NullExpression());		
+		while (! (temp.cdr() instanceof NullExpression)) {
+			temp = (ConsExpression) temp.cdr();
+			result = new ConsExpression(temp.car(), result);			
+		}
+		return result;
+	}
+	
+	public static ConsExpression append(ConsExpression items1, ConsExpression items2) {
+		ConsExpression temp = items1;		
+		while (! (temp.cdr() instanceof NullExpression)) {
+			items2 = new ConsExpression(temp.car(), items2);
+			temp = (ConsExpression) temp.cdr();
+		}
+		items2 = new ConsExpression(temp.car(), items2);
+		return items2;
+	}
+	
 	public String toString() {
 		return toString(this);
+	}
+	
+	public ConsExpression list() {
+		return null;
 	}
 	
 	public String toString(ConsExpression c) {
@@ -45,5 +97,17 @@ public class ConsExpression extends Expression{
 			}
 			return result;
 		}		
+	}
+	
+	public static void main(String[] args) {
+		String s = "(let ( ( l (cons 1 (cons 2 (cons 3 (cons 4 '())))))) (reverse l))";
+		//String s = "(append (cons 1 (cons 2 (cons 3 (cons 4 '() )))) (cons 1 '()))";
+		List<Token> tokens = Lexer.lex(s);
+		System.out.println("Tokens: " + tokens);
+		Program ast = Parser.parse(tokens);
+		String unparsed = Unparser.unparse(ast);
+		System.out.println(unparsed);
+		Expression v = Evaluator.evaluate(ast);
+		System.out.println("final result, evaluates to :  " + Unparser.unparse(v));
 	}
 }
